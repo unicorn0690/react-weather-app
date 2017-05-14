3ee5c28cfcdfb284962a9d6ed2d232d3d086ad3a
unicorn0690 <bathija.kapil@gmail.com>
2017-05-14 12:45:27 +0530

    Adds grid system to react weather app

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
+import { Link, IndexLink } from 'react-router'
 
 export default class Examples extends Component {
 	render () {
-		return <h3>Examples Page</h3>
+		return (
+			<div>
+				<h1 className='text-center'>Examples</h1>
+				<p>Here are few examples to try it out:</p>
+				<ol>
+					<li>
+						<Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+					</li>
+					<li>
+						<Link to='/?location=Kalyan'>Kalyan, KYN</Link>
+					</li>
+				</ol>
+			</div>
+		)
 	}
 }
