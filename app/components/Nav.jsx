d4ba72bb4e1c9979693e7ac2b54f1b0162034108
unicorn0690 <bathija.kapil@gmail.com>
2017-05-14 11:53:37 +0530

    Adds styling for nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,14 +2,39 @@ import React, { Component } from 'react'
 import { Link, IndexLink } from 'react-router'
 
 export default class Nav extends Component {
-  render() {
+
+    onSearch = (e) => {
+        e.preventDefault()
+        console.log(this.refs.location.value)
+    }
+
+    render() {
     return (
-    	<div>
-    		<h1>Nav Component</h1>
-    		<IndexLink to='/' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
-    		<Link to='/about' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>About</Link>
-    		<Link to='/examples' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
+    	<div className='top-bar'>
+            <div className='top-bar-left'>
+                <ul className='menu'>
+                    <li className='menu-text'>React Weather App</li>
+                    <li>
+                        <IndexLink to='/' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
+                    </li>
+                    <li>
+                        <Link to='/about' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/examples' activeClassName='active' activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className='top-bar-right'>
+                <form onSubmit={this.onSearch}>
+                    <ul className='menu'>
+                        <li><input type='search' placeholder='Search Weather' ref='location' /></li>
+                        <li><button className='button'>Get Weather</button></li>
+                    </ul>
+                </form>
+            </div>
+
     	</div>
-	)
-  }
+    )
+    }
 }
